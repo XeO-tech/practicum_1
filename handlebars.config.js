6353defceb2d86af74021d3f5689cd28ca30d6cf
XeO-tech <75488901+XeO-tech@users.noreply.github.com>
2022-12-04 16:53:15 +0300

    add structure

---
+++ handlebars.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  data: `./src/data`,
+  partials: `./src/partials`,
+  layouts: `./src/layouts`,
+  pages: `./src/pages`,
+};
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: 'jit',
+  content: ['./src/**/*.{hbs,html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        dark0: '#1e1e1e',
+        dark1: '#252526',
+      }
+    },
+  },
+  plugins: [],
+};
