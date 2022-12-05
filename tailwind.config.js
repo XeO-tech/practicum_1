98bb9be489c2b7518b4e1c1af14ebfab189b5019
XeO-tech <75488901+XeO-tech@users.noreply.github.com>
2022-12-05 22:37:40 +0300

    add design for few pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,29 @@ module.exports = {
   mode: 'jit',
   content: ['./src/**/*.{hbs,html,js}'],
   theme: {
+    fontFamily: {
+      monospace: ['IBM Plex Mono', 'Courier New', 'monospace'],
+    },
+    color: `#ffffff`,
     extend: {
       colors: {
-        dark0: '#1e1e1e',
-        dark1: '#252526',
-      }
+        dark0: '#1C1C28',
+        dark1: '#28293D',
+        dark2: '#32334D',
+        purple2: '#AC5DD9',
+        green0: '#05A660',
+        green1: '#06C270',
+        green2: '#39D98A',
+        green3: '#57EBA1',
+        teal1: '#00CFDE',
+        red0: '#E53535',
+      },
+      zIndex: {
+        '-1': '-1',
+      },
+      transformOrigin: {
+        0: '0%',
+      },
     },
   },
   plugins: [],
